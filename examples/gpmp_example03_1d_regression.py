correct computation of posterior variance

examples/gpmp_example03_1d_regression.py
@@ -23,7 +23,6 @@ import numpy as np
 import jax.numpy as jnp
 import gpmp as gp
 
-
 #-- dataset
 
 
@@ -42,15 +41,16 @@ def generate_data(noise_std):
     xt = gp.misc.designs.regulargrid(dim, nt, box)
     zt = gp.misc.testfunctions.twobumps(xt)
 
-    # ind = np.arange(nt)
+    # ind = np.arange(nt) 
     # np.random.shuffle(ind)
     # ni = 5
     # ind = ind[0:ni]
-    ind = [10, 45, 100, 130, 160]
+    ind = [10, 45, 100, 130, 130, 130, 131, 132, 133, 133, 133, 134, 160]
     xi = xt[ind]
-    zi = zt[ind] + noise_std * np.random.randn(5)
+    zi = zt[ind] + noise_std * np.random.randn(len(ind))
 
     return xt, zt, xi, zi
+
 
 noise_std = 1e-1
 xt, zt, xi, zi = generate_data(noise_std)
@@ -69,30 +69,39 @@ def constant_mean(x, param):
 mean = constant_mean
 
 
-def kernel(x, y, param):
+def kernel(x, y, param, pairwise=False):
 
     p = 2
     sigma2 = jnp.exp(param[0])
     invrho = jnp.exp(param[1])
     noise_variance = jnp.exp(param[2])
 
-    xs = gp.kernel.scale(x, invrho)
-    if y is x:
-        K = gp.kernel.distance(xs, xs)
-        K = sigma2 * gp.kernel.maternp_kernel(p, K) \
-            + noise_variance * jnp.eye(K.shape[0])
+    if y is x or y is None:
+        if pairwise:
+            K = sigma2 * jnp.ones((x.shape[0], ))  # nx x 0
+        else:
+            xs = gp.kernel.scale(x, invrho)
+            K = gp.kernel.distance(xs, xs)  # nx x nx
+            K = sigma2 * gp.kernel.maternp_kernel(p, K) \
+                + noise_variance * jnp.eye(K.shape[0])
     else:
+        xs = gp.kernel.scale(x, invrho)
         ys = gp.kernel.scale(y, invrho)
-        K = gp.kernel.distance(xs, ys)
+        if pairwise:
+            K = gp.kernel.distance_pairwise(xs, ys)  # nx x 0
+        else:
+            K = gp.kernel.distance(xs, ys)  # nx x ny
+            
         K = sigma2 * gp.kernel.maternp_kernel(p, K)
 
     return K
 
 
 meanparam = None
-covparam = jnp.array([math.log(0.5**2),        # log(sigma2)
-                      math.log(1 / .7),        # log(1/rho)
-                      2*math.log(noise_std)])  # log(noise_variance)
+covparam = jnp.array([
+    math.log(0.5**2),  # log(sigma2)
+    math.log(1 / .7),  # log(1/rho)
+    2 * math.log(noise_std)])  # log(noise_variance)
 model = gp.core.Model(mean, kernel, meanparam, covparam)
 
 #-- prediction

examples/gpmp_example04_1d_with_param_select.py
@@ -42,9 +42,9 @@ def constant_mean(x, param):
     return np.ones((x.shape[0], 1))
 
 
-def kernel(x, y, covparam):
+def kernel(x, y, covparam, pairwise=False):
     p = 3
-    return gp.kernel.maternp_covariance(x, y, p, covparam)
+    return gp.kernel.maternp_covariance(x, y, p, covparam, pairwise)
 
 
 meanparam = None

examples/gpmp_example06_nd.py
@@ -72,9 +72,9 @@ def constant_mean(x, param):
     return np.ones((x.shape[0], 1))
 
 
-def kernel(x, y, covparam):
+def kernel(x, y, covparam, pairwise=False):
     p = 10
-    return gp.kernel.maternp_covariance(x, y, p, covparam)
+    return gp.kernel.maternp_covariance(x, y, p, covparam, pairwise)
 
 
 model = gp.core.Model(constant_mean, kernel)

gpmp/core.py
@@ -47,7 +47,8 @@ class Model:
         Kit = self.covariance(xi, xt, self.covparam)
         lambda_t = linalg.solve(Kii, Kit, sym_pos=True, overwrite_a=True, overwrite_b=True)
 
-        zt_posterior_variance = Kii[0, 0] - jnp.einsum('i..., i...', lambda_t, Kit)
+        zt_prior_variance = self.covariance(xt, None, self.covparam, pairwise=True)
+        zt_posterior_variance = zt_prior_variance - jnp.einsum('i..., i...', lambda_t, Kit)
 
         return lambda_t, zt_posterior_variance
 
@@ -72,7 +73,8 @@ class Model:
 
         lambda_t = lambdamu_t[0:ni, :]
 
-        zt_posterior_variance = LHS[0, 0] - jnp.einsum('i..., i...', lambdamu_t, RHS)
+        zt_prior_variance = self.covariance(xt, None, self.covparam, pairwise=True)
+        zt_posterior_variance = zt_prior_variance - jnp.einsum('i..., i...', lambdamu_t, RHS)
 
         return lambda_t, zt_posterior_variance
         